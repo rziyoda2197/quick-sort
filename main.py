9cb11672c2c6043dc9344221a943e108b5ce7ffe
rziyoda2197 <rziyoda2197@gmail.com>
2026-04-20 23:50:38 +0500

    🚀 Add main application code

---
+++ main.py
@@ -0,0 +1,13 @@
+def quick_sort(arr):
+    if len(arr) <= 1:
+        return arr
+    pivot = arr[len(arr) // 2]
+    left = [x for x in arr if x < pivot]
+    middle = [x for x in arr if x == pivot]
+    right = [x for x in arr if x > pivot]
+    return quick_sort(left) + middle + quick_sort(right)
+
+# Test qilish
+print(quick_sort([3,6,8,10,1,2,1]))
+print(quick_sort([5,2,9,1,7,3]))
+print(quick_sort([10,9,8,7,6,5,4,3,2,1]))
